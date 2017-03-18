bb7b73fef34547bdb10eb6339e570f700d21839a
Dan Vingo <danvingo@gmail.com>
2017-03-18 19:49:00 -0400

    something

--- js/js.js
+++ js/js.js
@@ -11,13 +11,28 @@ window.m4 = m4, window.v3=v3
 const createEl = document.createElement.bind(document)
 Node.prototype.on = function(){this.addEventListener.apply(this,arguments)}
 
+const gameState = {
+  thrust: .01,
+  maxAcc: .03,
+  maxVelocity: 10,
+  rotateBy: Math.PI/40,
+  keys: {
+    ctrlPressed: false,
+    leftPressed: false,
+    rightPressed: false,
+    upPressed: false,
+    downPressed: false,
+    spacePressed: false
+  }
+}
+
 var gameTypes = objFromStrs('ship')
 /**
  * Game object.
  */
 function GObject() {
   this.position = v3.create(0,0,0)
-  this.scale = v3.create(2,2,2)
+  this.scale = v3.create(1, 1, 1)
   this.velocity = v3.create(0, 0, 0)
   this.acceleration = v3.create(0, 0, 0)
   this.rotateY = 0
@@ -29,8 +44,12 @@ function GObject() {
 
 var ship = new GObject()
 ship.position = v3.create(canvasSize.w / 2, canvasSize.h / 2, 0)
-ship.scale = v3.create(2,2,2)
+// ship.scale = v3.create(2,2,2)
+ship.scale = v3.create(.2,.2,.2)
+ship.rotateX= Math.PI/2
+ship.rotateY= Math.PI/2
 ship.type = gameTypes.ship
+ship.width = 15
 window.ship = ship
 
 const getJson = (endpoint, cb) => {
@@ -48,29 +67,55 @@ const getJson = (endpoint, cb) => {
 const loadModel = (file, cb) => {
   getJson(file, data => cb(data))
 }
+function isAccelerating(gameState) {
+  return gameState.keys.upPressed ||
+         gameState.keys.downPressed
+}
 
-const maxVelocity = 10
 function updateShip(gl, ship) {
-  //console.log('updating ship');
-  v3.add(ship.velocity, ship.acceleration, ship.velocity)
-  if (ship.velocity[0] > maxVelocity) ship.velocity[0] = maxVelocity
-  if (ship.velocity[1] > maxVelocity) ship.velocity[1] = maxVelocity
-  if (ship.velocity[0] < -maxVelocity) ship.velocity[0] = -maxVelocity
-  if (ship.velocity[1] < -maxVelocity) ship.velocity[1] = -maxVelocity
+  if (gameState.keys.upPressed) {
+    console.log('UP pressed');
+    ship.acceleration[0] += Math.cos(ship.rotateZ) * gameState.thrust
+    ship.acceleration[1] += Math.sin(ship.rotateZ) * gameState.thrust
+    if (ship.acceleration[0] > gameState.maxAcc) ship.acceleration[0] = gameState.maxAcc
+    if (ship.acceleration[1] > gameState.maxAcc) ship.acceleration[1] = gameState.maxAcc
+  }
+
+  if (gameState.keys.downPressed) {
+    console.log('DOWN pressed');
+    ship.acceleration[0] -= Math.cos(ship.rotateZ) * gameState.thrust
+    ship.acceleration[1] -= Math.sin(ship.rotateZ) * gameState.thrust
+    if (ship.acceleration[0] < -gameState.maxAcc) ship.acceleration[0] = -gameState.maxAcc
+    if (ship.acceleration[1] < -gameState.maxAcc) ship.acceleration[1] = -gameState.maxAcc
+  }
+  if (gameState.keys.leftPressed) {
+    ship.rotateZ += gameState.rotateBy
+    setV3Angle(ship.acceleration, ship.rotateZ)
+  }
+  if (gameState.keys.rightPressed) {
+    ship.rotateZ -= gameState.rotateBy
+    setV3Angle(ship.acceleration, ship.rotateZ)
+  }
+  if (isAccelerating(gameState)) {
+    v3.add(ship.velocity, ship.acceleration, ship.velocity)
+  } else {
+    setV3Angle(ship.acceleration, ship.rotateZ)
+  }
+  if (ship.velocity[0] > gameState.maxVelocity) ship.velocity[0] = gameState.maxVelocity
+  if (ship.velocity[1] > gameState.maxVelocity) ship.velocity[1] = gameState.maxVelocity
+  if (ship.velocity[0] < -gameState.maxVelocity) ship.velocity[0] = -gameState.maxVelocity
+  if (ship.velocity[1] < -gameState.maxVelocity) ship.velocity[1] = -gameState.maxVelocity
   v3.add(ship.position, ship.velocity, ship.position)
   // Wrap the ship around the screen.
   if (ship.position[0] + ship.width < 0) {
     ship.position[0] = gl.canvas.clientWidth + ship.width
   }
-  // Wrap the ship around the screen.
   if (ship.position[0] > gl.canvas.clientWidth + ship.width) {
     ship.position[0] = 0
   }
-
   if (ship.position[1] + ship.width < 0) {
     ship.position[1] = gl.canvas.clientHeight + ship.width
   }
-
   if (ship.position[1] > gl.canvas.clientHeight + ship.width) {
     ship.position[1] = 0
   }
@@ -146,14 +191,13 @@ function main(data) {
   var programInfo = twgl.createProgramInfo(gl, ['vs', 'fs'])
   window.bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays)
 
-  var view = m4.identity();
-
   const keyNames = {
     left: 37,
     right: 39,
     down: 40,
     up: 38,
-    space: 32
+    space: 32,
+    ctrl: 17
   }
 
   const keyToName = {
@@ -161,46 +205,36 @@ function main(data) {
     39: 'right',
     40: 'down',
     38: 'up',
-    32: 'space'
+    32: 'space',
+    17: 'ctrl'
   }
 
   const validKeys = Object.keys(keyToName).map(Number)
   window.uniforms = uniforms
   window.m4 = m4
-  window.addEventListener('keydown', throttle(({keyCode}) => {
-    if (validKeys.indexOf(keyCode) < 0) { return }
-    console.log('got keydown: ', keyToName[keyCode])
-    const keyName = keyToName[keyCode]
-    const thrust = .01
-    const maxAcc = .03
-    if (keyCode === keyNames.up) {
-      ship.acceleration[0] += Math.cos(ship.rotateZ) * thrust
-      ship.acceleration[1] += Math.sin(ship.rotateZ) * thrust
-      if (ship.acceleration[0] > maxAcc) ship.acceleration[0] = maxAcc
-      if (ship.acceleration[1] > maxAcc) ship.acceleration[1] = maxAcc
-    }
-    if (keyCode === keyNames.down) {
-      ship.acceleration[0] -= Math.cos(ship.rotateZ) * thrust
-      ship.acceleration[1] -= Math.sin(ship.rotateZ) * thrust
-      if (ship.acceleration[0] < -maxAcc) ship.acceleration[0] = -maxAcc
-      if (ship.acceleration[1] < -maxAcc) ship.acceleration[1] = -maxAcc
-    }
-    if (keyCode === keyNames.left) {
-      // ship.rotateY +=  Math.PI/12
-      ship.rotateZ +=  Math.PI/12
-      setV3Angle(ship.acceleration, ship.rotateZ)
-    }
-    if (keyCode === keyNames.right) {
-      //ship.rotateY -= Math.PI/12
-      ship.rotateZ -= Math.PI/12
-      setV3Angle(ship.acceleration, ship.rotateZ)
-    }
-    console.log('ship.acc: ', ship.acceleration);
-  }, 30))
 
-  var shipPoint = [arrays.position.data[0], arrays.position.data[1], arrays.position.data[2]]
-  var shipLoc = [], shipViewLoc = []
-  var viewProjectInverse = m4.identity()
+  window.addEventListener('keyup', (e) => {
+    // e.preventDefault()
+    var keyCode = e.keyCode
+    console.log('keuyp', keyCode);
+    if (keyCode == keyNames.up) gameState.keys.upPressed = false
+    if (keyCode == keyNames.down) gameState.keys.downPressed = false
+    if (keyCode == keyNames.left) gameState.keys.leftPressed = false
+    if (keyCode == keyNames.right) gameState.keys.rightPressed = false
+    if (keyCode == keyNames.space) gameState.keys.spacePressed = false
+    if (keyCode == keyNames.ctrl) gameState.keys.ctrlPressed = false
+  })
+
+  window.addEventListener('keydown', (e) => {
+    // e.preventDefault()
+    var keyCode = e.keyCode
+    console.log('got keydown: ', keyCode)
+    if (validKeys.indexOf(keyCode) < 0) { return }
+    if (keyCode == keyNames.up) gameState.keys.upPressed = true
+    if (keyCode == keyNames.down) gameState.keys.downPressed = true
+    if (keyCode == keyNames.left) gameState.keys.leftPressed = true
+    if (keyCode === keyNames.right) gameState.keys.rightPressed = true
+  })
 
   /**
    * Camera setup.
@@ -210,14 +244,6 @@ function main(data) {
   var eye = [0, 0, .1], target = [0, 0, 0], up = [0, 1, 0]
   var left = 0, right = gl.canvas.clientWidth, bottom = gl.canvas.clientHeight,
       top = 0, near = -40, far = 40
-  var projection = m4.ortho(left, right, top, bottom, near, far)
-  console.log('projection: ', projection);
-  view = m4.identity()
-  uniforms.u_viewProjection = m4.multiply(projection, view)
-
-  var clipSpaceCoord = m4.transformPoint(uniforms.u_viewProjection, shipPoint)
-  console.log('ship point: ', shipPoint);
-  console.log('clip space coord: ', clipSpaceCoord);
 
   /**
    * Draw.
@@ -226,13 +252,11 @@ function main(data) {
     twgl.resizeCanvasToDisplaySize(gl.canvas)
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
     const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight
-
     var left = 0, right = gl.canvas.clientWidth, bottom = gl.canvas.clientHeight,
         top = 0, near = -10, far = 10
     var projection = m4.ortho(left, right, top, bottom, near, far)
+    var view = m4.identity()
     uniforms.u_viewProjection = m4.multiply(projection, view)
-
-    m4.inverse(uniforms.u_viewProjection, viewProjectInverse)
     uniforms.u_model = update(gl, ship)
 
     gl.useProgram(programInfo.program)
@@ -242,13 +266,12 @@ function main(data) {
     gl.clearColor(0, 0, 0, 1)
     gl.clear(gl.COLOR_BUFFER_BIT)
     gl.drawElements(gl.TRIANGLES, bufferInfo.numElements, gl.UNSIGNED_SHORT, 0)
-    // twgl.drawBufferInfo(gl, bufferInfo, gl.TRIANGLES)
-
     requestAnimationFrame(render)
   }
   requestAnimationFrame(render)
 }
 
-loadModel('models/shipRotatedYUp.json', (data) => {
+loadModel('models/plane.json', (data) => {
+// loadModel('models/shipRotatedYUp.json', (data) => {
   main(data)
 })
